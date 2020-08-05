f31c3d5f11b3581e93b87db4f0d797d448977c13
adrimual <adrimual14@gmail.com>
2020-08-05 19:51:58 +0200

    estructura arreglada

--- routes/logout.js
+++
@@ -1,9 +0,0 @@
-/* var express = require('express');
-var router = express.Router();
-// GET '/logout'
-router.get('/logout', (req, res, next) => {
-  req.session.destroy( (err) => {
-    res.redirect('/');
-  })
-});
-module.exports = router; */
